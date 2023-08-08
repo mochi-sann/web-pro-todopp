ac63ed586e41c263794bfce95e7f2fea7fc0b6d8
mochi33i <44772513+mochi-sann@users.noreply.github.com>
2023-08-08 21:53:37 +0900

    todo のdoneを更新できるようになった

--- app/front/src/components/TodoItem.tsx
+++ app/front/src/components/TodoItem.tsx
@@ -1,19 +1,51 @@
 "use client";
 
-import React from "react";
+import React, { useState } from "react";
 import { TodoDto } from "../../api/@types";
 import { Checkbox } from "./ui/checkbox";
+import { AspidaClient } from "@/lib/utils";
+import { useToast } from "./ui/use-toast";
+import { title } from "process";
 
-export type TodoItemProps = {
+export interface TodoItemProps {
   todoItem: TodoDto;
-};
-
+}
 export const TodoItem: React.FC<TodoItemProps> = (props) => {
+  const { todoItem } = props;
+
+  const [checked, setChecked] = useState(props.todoItem.done);
+  const { toast } = useToast();
+  const onCheckedChange = async (e: boolean) => {
+    setChecked(e);
+    const checkupdate = await AspidaClient.todo._id(props.todoItem.id).patch({
+      body: {
+        done: e,
+        text: props.todoItem.text,
+        description: props.todoItem.description,
+      },
+    });
+    toast({
+      title: "update",
+      description: JSON.stringify(checkupdate),
+    });
+  };
   return (
-    <div className="border-gray-500 border-2 rounded p-4">
-      <Checkbox />
-      <p className="text-bold">{props.todoItem.text}</p>
-      <p className="text-bold">{props.todoItem.description}</p>
+    <div
+      className={`border-gray-500 border-2 rounded p-4 flex flex-row items-center gap-2 ${
+        checked ? "text-slate-400 line-through" : "text-slate-950"
+      }`}
+    >
+      <div className="flex">
+        <Checkbox
+          id={`TodoCheckBox.${todoItem.id.toString()}`}
+          defaultChecked={props.todoItem.done}
+          onCheckedChange={(e: boolean) => onCheckedChange(e)}
+        />
+      </div>
+      <div>
+        <p className="font-bold text-lg">{todoItem.text}</p>
+        <p className="">{props.todoItem.description}</p>
+      </div>
     </div>
   );
 };
--- app/front/src/components/newTodo.tsx
+++ app/front/src/components/newTodo.tsx
@@ -32,9 +32,10 @@ export const NewTodo = () => {
   async function onSubmit(values: z.infer<typeof formSchema>) {
     // Do something with the form values.
     // ✅ This will be type-safe and validated.
-    await AspidaClient.todo.post({
+    const data = await AspidaClient.todo.post({
       body: { text: values.text, description: values.description, done: false },
     });
+    console.log(data);
   }
 
   return (
