1924e5c35e1abc59d463cdbea7aa09326046b1db
mochi33i <44772513+mochi-sann@users.noreply.github.com>
2023-08-08 13:22:46 +0900

    frontアプリを導入した

--- app/api/src/main.ts
+++ app/api/src/main.ts
@@ -5,6 +5,6 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     logger: ['error', 'warn', 'debug', 'verbose', 'log'],
   });
-  await app.listen(3000);
+  await app.listen(3001);
 }
 bootstrap();
